6b00016a3db8582bdd7ece0b038a86e5bcfa8250
Sean Bryant <smbryant1730@gmail.com>
2023-02-09 20:54:45 -0500

    created navBar

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ const rendertoDom = (divId, htmlToRender) => {
   
     selectedDiv.innerHTML = htmlToRender; 
   };
+
+// created navBar html function
+const navBarOnDom = () => {
+  let navString = `<div class="btnContainer">
+  <button class="btn btn-primary" id="ovrBtn">Overview</button>
+  <button class="btn btn-primary" id="repBtn">Repositories</button>
+  <button class="btn btn-primary" id="projBtn">Projects</button>
+  <button class="btn btn-primary" id="pkgBtn">Packages</button>
+</div>`
+renderToDom("#nav-bar-container", navString);
+};
+
 //making form
 const formOnDom = () => {
     const headerOnDom = `<form>
@@ -20,6 +32,7 @@ const formOnDom = () => {
   }
   const startApp =() => {
     formOnDom()
+    navBarOnDom()
   };
 
   startApp();
