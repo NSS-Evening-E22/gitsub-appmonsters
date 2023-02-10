d12bb770abf1132db10073e6fb068b6046cc0668
coleama <cole.ama@gmail.com>
2023-02-09 19:10:59 -0600

    created-form

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("linked!");
+const rendertoDom = (divId, htmlToRender) => {
+    const selectedDiv = document.querySelector(divId);
+  
+    selectedDiv.innerHTML = htmlToRender; 
+  };
+//making form
+const formOnDom = () => {
+    const headerOnDom = `<form>
+    <h1>Create New Project</h1>
+    <label class="sr-only">Project Title: </label>
+    <input type="text" class="form-control" id="inputTitle" placeholder="Title of Project">
+    <label class="sr-only">Project Descripton: </label>
+    <input type="text" class="form-control" id="inputDescription" placeholder="Project Description">
+  </form>
+  </div>
+  <button id="btnSubmitRepo" class="btn btn-primary">Submit</button>`;
+  
+  rendertoDom('#form-container', headerOnDom);
+  }
+  const startApp =() => {
+    formOnDom()
+  };
+
+  startApp();
