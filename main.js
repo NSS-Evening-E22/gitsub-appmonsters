e4ec6ead561727b25af78b418497faaa87e23ca4
Sean Bryant <smbryant1730@gmail.com>
2023-02-09 20:15:55 -0500

    created render function, created startup function, and created navbar html

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+// created renderToDom function
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+// created navBar html function
+const navBarOnDom = () => {
+  let navString = `<div class="btnContainer">
+  <button class="btn btn-primary" id="ovrBtn">Overview</button>
+  <button class="btn btn-primary" id="repBtn">Repositories</button>
+  <button class="btn btn-primary" id="projBtn">Projects</button>
+  <button class="btn btn-primary" id="pkgBtn">Packages</button>
+</div>`
+renderToDom("#nav-bar-container", navString);
+};
+
+// created startApp function
+const startApp = () => {
+  navBarOnDom()
+};
+
 console.log("linked!");
+
+startApp()
